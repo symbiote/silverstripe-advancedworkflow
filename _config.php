daee0cd795b5fac0268d0ac2be8e97a29fc87a2b
Andrew Short <andrewjshort@gmail.com>
2010-10-04 04:46:03 +1100

    MINOR: Removed unneccesary MemberHierarchy extension - the relationship is already created by Hierarchy.

--- _config.php
+++ _config.php
@@ -3,7 +3,5 @@
 // Add the following to your config to enable workflow 
 // DataObject::add_extension('SiteTree', 'WorkflowApplicable');
 
-Object::add_extension('Member', 'MemberHierarchy');
 Object::add_extension('Member', 'Hierarchy');
-
 Object::add_extension('LeftAndMain', 'ActivityWorkflowExtension');
