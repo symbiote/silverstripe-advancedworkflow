bf022c73ac6693d503f195a3a57b5ed030937961
Andrew Short <andrewjshort@gmail.com>
2010-10-04 13:06:52 +1100

    FEATURE: Implemented NotifyUsersWorkflowAction, which sends an email to all users attached to a workflow object.

--- code/actions/NotifyUsersWorkflowAction.php
+++ code/actions/NotifyUsersWorkflowAction.php
@@ -1,34 +1,114 @@
 <?php
-/* 
- * 
-All code covered by the BSD license located at http://silverstripe.org/bsd-license/
- */
-
 /**
- * Workflow action that notifies users on the workflow that they
- * have a task waiting for them
+ * A workflow action that notifies users attached to the workflow path that they have a task awaiting them.
  *
- * @author marcus@silverstripe.com.au
+ * @todo       Show the CMS user what fields are available in the email.
+ * @license    BSD License (http://silverstripe.org/bsd-license/)
+ * @package    activityworkflow
+ * @subpackage actions
  */
 class NotifyUsersWorkflowAction extends WorkflowAction {
 
-	public static $icon = 'activityworkflow/images/notify.png';
-	
-    public static $db = array(
-		'Subject' => 'Varchar(64)',
-		'NotificationText' => 'Text',
+	public static $db = array(
+		'EmailSubject'  => 'Varchar(100)',
+		'EmailFrom'     => 'Varchar(50)',
+		'EmailTemplate' => 'Text'
 	);
+
+	public static $icon = 'activityworkflow/images/notify.png';
 
 	public function getCMSFields() {
 		$fields = parent::getCMSFields();
-		$fields->addFieldToTab('Root.Main', new TextField('Subject', _t('NotifyUsersWorkflowAction.SUBJECT', 'Subject')));
-		$fields->addFieldToTab('Root.Main', new TextareaField('NotificationText', _t('NotifyUsersWorkflowAction.MESSAGE', 'Message')));
+
+		$fields->addFieldsToTab('Root.Main', array(
+			new HeaderField('NotificationEmail', $this->fieldLabel('NotificationEmail')),
+			new LiteralField('NotificationNote', '<p>' . $this->fieldLabel('NotificationNote') . '</p>'),
+			new TextField('EmailSubject', $this->fieldLabel('EmailSubject')),
+			new TextField('EmailFrom', $this->fieldLabel('EmailFrom')),
+			new TextareaField('EmailTemplate', $this->fieldLabel('EmailTemplate'))
+		));
+
 		return $fields;
 	}
 
+	public function fieldLabels() {
+		return array_merge(parent::fieldLabels(), array(
+			'NotificationEmail' => _t('ActivityWorkfow.NOTIFICATIONEMAIL', 'Notification Email'),
+			'NotificationNote'  => _t('ActivityWorkfow.NOTIFICATIONNOTE',
+				'All users attached to the workflow will be sent an email when this action is run.'),
+			'EmailSubject'      => _t('ActivityWorkfow.EMAILSUBJECT', 'Email subject'),
+			'EmailFrom'         => _t('ActivityWorkfow.EMAILFROM', 'Email from'),
+			'EmailTemplate'     => _t('ActivityWorkfow.EMAILTEMPLATE', 'Email template')
+		));
+	}
+
 	public function execute() {
-		// send the emails to everyone
+		$email   = new Email;
+		$members = $this->Workflow()->getAssignedMembers();
+		$emails  = '';
+
+		if(!$members || !count($members)) return;
+
+		foreach($members as $member) {
+			if($member->Email) $emails .= "$member->Email, ";
+		}
+
+		$context   = $this->getContextFields();
+		$member    = $this->getMemberFields();
+		$variables = array();
+
+		foreach($context as $field => $val) $variables["\$Context.$field"] = $val;
+		foreach($member as $field => $val)  $variables["\$Member.$field"] = $val;
+
+		$subject = str_replace(array_keys($variables), array_values($variables), $this->EmailSubject);
+		$body    = str_replace(array_keys($variables), array_values($variables), $this->EmailTemplate);
+
+		$email->setSubject($subject);
+		$email->setFrom($this->EmailFrom);
+		$email->setBcc(substr($emails, 0, -2));
+		$email->setBody($body);
+		$email->send();
 
 		return true;
 	}
+
+	/**
+	 * @return array
+	 */
+	public function getContextFields() {
+		if(!$this->Workflow()->getContext()) return array();
+
+		$record = $this->Workflow()->getContext();
+		$fields = $record->summaryFields();
+		$result = array();
+
+		foreach($fields as $field) {
+			$result[$field] = $record->$field;
+		}
+
+		if($record instanceof SiteTree) {
+			$result['CMSLink'] = singleton('CMSMain')->Link("show/{$record->ID}");
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getMemberFields() {
+		$member = Member::currentUser();
+		$result = array();
+
+		if($member) foreach($member->summaryFields() as $field => $title) {
+			$result[$field] = $member->$field;
+		}
+
+		if($member && !array_key_exists('Name', $result)) {
+			$result['Name'] = $member->getName();
+		}
+
+		return $result;
+	}
+
 }
