39b3d1051520934e8acf4e40ef1b5de4dd3b0b6f
Shea <shea@silverstripe.com.au>
2012-11-20 16:12:17 +1100

    MINOR display publish and unpublish dates and times as read only fields

--- code/extensions/WorkflowEmbargoExpiryExtension.php
+++ code/extensions/WorkflowEmbargoExpiryExtension.php
@@ -39,8 +39,13 @@ class WorkflowEmbargoExpiryExtension extends DataExtension {
 		
 		if ($effective) {
 			$fields->addFieldsToTab('Root.PublishingSchedule', array(
-				$dt = new Datetimefield('DesiredPublishDate', _t('AdvancedWorkflow.REQUESTED_PUBLISH_DATE', 'Requested dublish date and time')),
+				$dt = new Datetimefield('DesiredPublishDate', _t('AdvancedWorkflow.REQUESTED_PUBLISH_DATE', 'Requested publish date and time')),
 				$ut = new Datetimefield('DesiredUnPublishDate', _t('AdvancedWorkflow.REQUESTED_UNPUBLISH_DATE', 'Requested un-publish date and time')),
+			));
+
+			$fields->addFieldsToTab('Root.PublishingSchedule', array(
+				Datetimefield::create('PublishOnDate', _t('AdvancedWorkflow.PUBLISH_ON', 'Publish date and time'))->setDisabled(true),
+				Datetimefield::create('UnPublishOnDate', _t('AdvancedWorkflow.UNPUBLISH_ON', 'Un-publish date and time'))->setDisabled(true),
 			));
 		} else {
 			$fields->addFieldsToTab('Root.PublishingSchedule', array(
