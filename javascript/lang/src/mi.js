0f51f6e283bce12d9cb9330860f8b415ae3787bb
Ingo Schommer <ingo@silverstripe.com>
2013-11-14 23:07:41 +0100

    Translations

---
+++ javascript/lang/src/mi.js
@@ -0,0 +1,7 @@
+{
+    "Workflow.EMBARGOMESSAGETIME": "Ka whakaputa aunoatia anō i tēnei rā ngā hukihuki kua tiakina o tēnei whārangi <a>%s i %s</a>",
+    "Workflow.EMBARGOMESSAGEDATETIME": "Ka whakaputa aunoatia ngā hukihuki kua tiakina o tēnei whārangi <a>%s i %s</a>",
+    "Workflow.EMBARGOMESSAGEDATE": "Ka whakaputa aunoatia ngā hukihuki kua tiakina o tēnei whārangi <a>%s i %s</a>",
+    "Workflow.DeleteQuestion": "Me muku pūmau tēnei?",
+    "Workflow.ProcessError": "Kāore i taea te tukatuka te reremahi"
+}
