5364cbe184a4cef1748c9b85515b6ddabe182a57
Mateusz Uzdowski <mateusz@silverstripe.com>
2014-01-24 14:37:00 +1300

    Add new lang strings, convert to JS.

--- javascript/lang/mi.js
+++ javascript/lang/mi.js
@@ -4,10 +4,10 @@ if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
 	if(typeof(console) != 'undefined') console.error('Class ss.i18n not defined');
 } else {
 	ss.i18n.addDictionary('mi', {
-    "Workflow.EMBARGOMESSAGETIME": "Ka whakaputa aunoatia anō i tēnei rā ngā hukihuki kua tiakina o tēnei whārangi <a>%s i %s</a>",
-    "Workflow.EMBARGOMESSAGEDATETIME": "Ka whakaputa aunoatia ngā hukihuki kua tiakina o tēnei whārangi <a>%s i %s</a>",
-    "Workflow.EMBARGOMESSAGEDATE": "Ka whakaputa aunoatia ngā hukihuki kua tiakina o tēnei whārangi <a>%s i %s</a>",
     "Workflow.DeleteQuestion": "Me muku pūmau tēnei?",
+    "Workflow.EMBARGOMESSAGETIME": "Ka whakaputa aunoatia anō i tēnei rā ngā hukihuki kua tiakina o tēnei whārangi <a>%s i %s</a>",
+    "Workflow.EMBARGOMESSAGEDATE": "Ka whakaputa aunoatia ngā hukihuki kua tiakina o tēnei whārangi <a>%s i %s</a>",
+    "Workflow.EMBARGOMESSAGEDATETIME": "Ka whakaputa aunoatia ngā hukihuki kua tiakina o tēnei whārangi <a>%s i %s</a>",
     "Workflow.ProcessError": "Kāore i taea te tukatuka te reremahi"
 });
 }
--- javascript/lang/zh.js
+++ javascript/lang/zh.js
@@ -4,10 +4,10 @@ if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
 	if(typeof(console) != 'undefined') console.error('Class ss.i18n not defined');
 } else {
 	ss.i18n.addDictionary('zh', {
-    "Workflow.EMBARGOMESSAGETIME": "这个页面已保存的草稿会在今天<a>%s</a>自动发布",
-    "Workflow.EMBARGOMESSAGEDATETIME": "这个页面已保存的草稿会在<a>%s  %s</a>自动发布",
-    "Workflow.EMBARGOMESSAGEDATE": "这个页面已保存的草稿会在<a>%s</a>自动发布",
     "Workflow.DeleteQuestion": "您确定要永久删除这项吗？",
+    "Workflow.EMBARGOMESSAGETIME": "这个页面已保存的草稿会在今天<a>%s</a>自动发布",
+    "Workflow.EMBARGOMESSAGEDATE": "这个页面已保存的草稿会在<a>%s</a>自动发布",
+    "Workflow.EMBARGOMESSAGEDATETIME": "这个页面已保存的草稿会在<a>%s  %s</a>自动发布",
     "Workflow.ProcessError": "无法处理工作流程"
 });
 }
