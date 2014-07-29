69ba3c4b545af5c329d0459f428bec37e2c867be
Stig Lindqvist <stojg.lindqvist@gmail.com>
2014-07-29 17:57:40 +1200

    NEW Adding a notify by email action
    This action allows a workflow to send an email to a email address that might not necessary be a user in the system, i.e. a shared email inbox

---
+++ code/actions/NotifyByEmailWorkflowAction.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ * A workflow action that notifies sends a notification email to one email address, not necessary
+ * a user in the system.
+ *
+ * @license    BSD License (http://silverstripe.org/bsd-license/)
+ * @package    advancedworkflow
+ * @subpackage actions
+ */
+class NotifyByEmailWorkflowAction extends WorkflowAction {
+
+	/**
+	 * @var array
+	 */
+	private static $db = array(
+		'EmailTo'               => 'Varchar(200)',
+		'EmailSubject'			=> 'Varchar(100)',
+		'EmailFrom'				=> 'Varchar(50)',
+		'EmailTemplate'			=> 'Text',
+		'ListingTemplateID'		=> 'Int',
+	);
+
+	/**
+	 * @var string
+	 */
+	public static $icon = 'advancedworkflow/images/notify.png';
+
+	/**
+	 * @return FieldList
+	 */
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+
+		$fields->addFieldsToTab('Root.Main', array(
+			new HeaderField('NotificationEmail', $this->fieldLabel('NotificationEmail')),
+			new LiteralField('NotificationNote', '<p>' . $this->fieldLabel('NotificationNote') . '</p>'),
+			new TextField('EmailTo', $this->fieldLabel('EmailTo')),
+			new TextField('EmailSubject', $this->fieldLabel('EmailSubject')),
+			new TextField('EmailFrom', $this->fieldLabel('EmailFrom')),
+			new TextareaField('EmailTemplate', $this->fieldLabel('EmailTemplate')),
+			new ToggleCompositeField('FormattingHelpContainer',
+				$this->fieldLabel('FormattingHelp'), new LiteralField('FormattingHelp', $this->getFormattingHelp()))
+		));
+
+		return $fields;
+	}
+
+	/**
+	 * @param bool $relations
+	 * @return array|string
+	 */
+	public function fieldLabels($relations = true) {
+		return array_merge(parent::fieldLabels($relations), array(
+			'NotificationEmail' => _t('NotifyUsersWorkflowAction.NOTIFICATIONEMAIL', 'Notification Email'),
+			'NotificationNote'  => _t('NotifyUsersWorkflowAction.NOTIFICATIONBYEMAILNOTE',
+				'Will send a notification email to the address in the \'Email to\' field'),
+			'EmailTo' => _t('NotifyUsersWorkflowAction.EmailTo', 'Email to'),
+			'EmailSubject'      => _t('NotifyUsersWorkflowAction.EMAILSUBJECT', 'Email subject'),
+			'EmailFrom'         => _t('NotifyUsersWorkflowAction.EMAILFROM', 'Email from'),
+			'EmailTemplate'     => _t('NotifyUsersWorkflowAction.EMAILTEMPLATE', 'Email template'),
+			'FormattingHelp'    => _t('NotifyUsersWorkflowAction.FORMATTINGHELP', 'Formatting Help')
+		));
+	}
+
+	/**
+	 * @param WorkflowInstance $workflow
+	 * @return bool
+	 */
+	public function execute(WorkflowInstance $workflow) {
+		$context   = $this->getContextFields($workflow->getTarget());
+		$member    = $this->getMemberFields();
+		$initiator = $this->getMemberFields($workflow->Initiator());
+		$variables = array();
+		
+		foreach($context as $field => $val) $variables["\$Context.$field"] = $val;
+		foreach($member as $field => $val)  $variables["\$Member.$field"] = $val;
+		foreach($initiator as $field => $val)  $variables["\$Initiator.$field"] = $val;
+
+		$pastActions = $workflow->Actions()->sort('Created DESC');
+		$variables["\$CommentHistory"] = $this->customise(array(
+			'PastActions'=>$pastActions,
+			'Now'=>SS_Datetime::now()
+		))->renderWith('CommentHistory');
+
+		$subject = str_replace(array_keys($variables), array_values($variables), $this->EmailSubject);
+
+		$item = $workflow->customise(array(
+			'Items'		=> $workflow->Actions(),
+			'Member'	=> Member::currentUser(),
+			'Context'	=> $workflow->getTarget(),
+			'CommentHistory' => $variables["\$CommentHistory"]
+		));
+		
+		$view = SSViewer::fromString($this->EmailTemplate);
+
+		$body = $view->process($item);
+		$from = str_replace(array_keys($variables), array_values($variables), $this->EmailFrom);
+		$to = str_replace(array_keys($variables), array_values($variables), $this->EmailTo);
+
+		$email = new Email;
+		$email->setTo($to);
+		$email->setSubject($subject);
+		$email->setFrom($from);
+		$email->setBody($body);
+		$email->send();
+
+		return true;
+	}
+
+	/**
+	 * @param  DataObject $target
+	 * @return array
+	 */
+	public function getContextFields(DataObject $target) {
+		$fields = $target->inheritedDatabaseFields();
+		$result = array();
+
+		foreach($fields as $field => $fieldDesc) {
+			$result[$field] = $target->$field;
+		}
+
+		if($target instanceof CMSPreviewable) {
+			$result['CMSLink'] = $target->CMSEditLink();
+		} else if ($target->hasMethod('WorkflowLink')) {
+			$result['CMSLink'] = $target->WorkflowLink();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Builds an array with the member information
+	 * @param Member $member An optional member to use. If null, will use the current logged in member
+	 * @return array
+	 */
+	public function getMemberFields(Member $member = null) {
+		if (!$member){
+			$member = Member::currentUser();
+		}
+		$result = array();
+
+		if($member) foreach($member->summaryFields() as $field => $title) {
+			$result[$field] = $member->$field;
+		}
+
+		if($member && !array_key_exists('Name', $result)) {
+			$result['Name'] = $member->getName();
+		}
+
+		return $result;
+	}
+
+
+	/**
+	 * Returns a basic set of instructions on how email templates are populated with variables.
+	 *
+	 * @return string
+	 */
+	public function getFormattingHelp() {
+		$note = _t('NotifyUsersWorkflowAction.FORMATTINGNOTE',
+			'Notification emails can contain HTML formatting. The following special variables are replaced with their
+			respective values in the email subject, email from and template/body.');
+		$member = _t('NotifyUsersWorkflowAction.MEMBERNOTE',
+			'These fields will be populated from the member that initiates the notification action. For example,
+			{$Member.FirstName}.');
+		$initiator = _t('NotifyUsersWorkflowAction.INITIATORNOTE',
+			'These fields will be populated from the member that initiates the workflow request. For example,
+			{$Initiator.Email}.');
+		$context = _t('NotifyUsersWorkflowAction.CONTEXTNOTE',
+			'Any summary fields from the workflow target will be available. For example, {$Context.Title}.
+			Additionally, the {$Context.AbsoluteEditLink} variable will contain a link to edit the workflow target in
+			the CMS (if it is a Page), and the {$Context.LinkToPendingItems} variable will generate a link to the CMS\' workflow admin,
+			useful for allowing users to enact workflow transitions, directly from emails.');
+		$fieldName = _t('NotifyUsersWorkflowAction.FIELDNAME', 'Field name');
+		$commentHistory = _t('NotifyUsersWorkflowAction.COMMENTHISTORY', 'Comment history up to this notification.');
+
+		$memberFields = implode(', ', array_keys($this->getMemberFields()));
+
+		return "<p>$note</p>
+			<p><strong>{\$Member.($memberFields)}</strong><br>$member</p>
+			<p><strong>{\$Initiator.($memberFields)}</strong><br>$initiator</p>
+			<p><strong>{\$Context.($fieldName)}</strong><br>$context</p>
+			<p><strong>{\$CommentHistory}</strong><br>$commentHistory</p>";
+	}
+
+}
