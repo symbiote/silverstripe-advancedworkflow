bad78eeabb5fe03d909d6253a6e1857f4d294810
Damian Mooyman <damian@silverstripe.com>
2014-08-06 17:48:01 +1200

    BUG Ensure that past dates for embargo / expiry are handled elegantly Tests also consider publish > unpublish and unpublish > publish workflows

--- tests/WorkflowEmbargoExpiryTest.php
+++ tests/WorkflowEmbargoExpiryTest.php
@@ -6,6 +6,17 @@
  */
 class WorkflowEmbargoExpiryTest extends SapphireTest {
 
+	public function setUp() {
+		parent::setUp();
+
+		SS_Datetime::set_mock_now('2014-01-05 12:00:00');
+	}
+
+	public function tearDown() {
+		SS_Datetime::clear_mock_now();
+		parent::tearDown();
+	}
+
 	/**
 	 * @var array
 	 */
@@ -31,7 +42,7 @@ class WorkflowEmbargoExpiryTest extends SapphireTest {
 		}
 		parent::__construct();
 	}
-	
+
 	public function testFutureDatesJobs() {
 		$page = new Page();
 
@@ -44,6 +55,15 @@ class WorkflowEmbargoExpiryTest extends SapphireTest {
 
 		$this->assertTrue($page->PublishJobID > 0);
 		$this->assertTrue($page->UnPublishJobID > 0);
+
+		// Check date ranges
+		$now = strtotime(SS_Datetime::now()->getValue());
+		$publish = strtotime($page->PublishJob()->StartAfter);
+		$unPublish = strtotime($page->UnPublishJob()->StartAfter);
+
+		$this->assertGreaterThan($now, $publish);
+		$this->assertGreaterThan($now, $unPublish);
+		$this->assertGreaterThan($publish, $unPublish);
 	}
 
 	public function testDesiredRemovesJobs() {
@@ -59,6 +79,15 @@ class WorkflowEmbargoExpiryTest extends SapphireTest {
 		$this->assertTrue($page->PublishJobID > 0);
 		$this->assertTrue($page->UnPublishJobID > 0);
 
+		// Check date ranges
+		$now = strtotime(SS_Datetime::now()->getValue());
+		$publish = strtotime($page->PublishJob()->StartAfter);
+		$unPublish = strtotime($page->UnPublishJob()->StartAfter);
+
+		$this->assertGreaterThan($now, $publish);
+		$this->assertGreaterThan($now, $unPublish);
+		$this->assertGreaterThan($publish, $unPublish);
+
 		$page->DesiredPublishDate = '2020-02-01 00:00:00';
 		$page->DesiredUnPublishDate = '2020-02-01 02:00:00';
 
@@ -67,11 +96,11 @@ class WorkflowEmbargoExpiryTest extends SapphireTest {
 		$this->assertTrue($page->PublishJobID == 0);
 		$this->assertTrue($page->UnPublishJobID == 0);
 	}
-	
+
 	public function testPublishActionWithFutureDates() {
 		$action = new PublishItemWorkflowAction();
 		$instance = new WorkflowInstance();
-		
+
 		$page = new Page();
 		$page->Title = 'stuff';
 		$page->DesiredPublishDate = '2020-02-01 00:00:00';
@@ -83,33 +112,124 @@ class WorkflowEmbargoExpiryTest extends SapphireTest {
 		$instance->TargetID = $page->ID;
 
 		$action->execute($instance);
-		
+
 		$page = DataObject::get_by_id('Page', $page->ID);
 		$this->assertTrue($page->PublishJobID > 0);
 		$this->assertTrue($page->UnPublishJobID > 0);
+
+		// Check date ranges
+		$now = strtotime(SS_Datetime::now()->getValue());
+		$publish = strtotime($page->PublishJob()->StartAfter);
+		$unPublish = strtotime($page->UnPublishJob()->StartAfter);
+
+		$this->assertGreaterThan($now, $publish);
+		$this->assertGreaterThan($now, $unPublish);
+		$this->assertGreaterThan($publish, $unPublish);
 	}
-	
-	protected function createDefinition() {
-		$definition = new WorkflowDefinition();
-		$definition->Title = "Dummy Workflow Definition";
-		$definition->write();
 
-		$stepOne = new WorkflowAction();
-		$stepOne->Title = "Step One";
-		$stepOne->WorkflowDefID = $definition->ID;
-		$stepOne->write();
+	/**
+	 * Test that a page with a past publish date creates the correct jobs
+	 */
+	public function testPastPublishThenUnpublish() {
+		$page = new Page();
+		$page->Title = 'My Page';
+		$page->write();
 
-		$stepTwo = new WorkflowAction();
-		$stepTwo->Title = "Step Two";
-		$stepTwo->WorkflowDefID = $definition->ID;
-		$stepTwo->write();
+		// No publish
+		$this->assertEmpty($page->PublishJobID);
+		$this->assertEmpty($page->UnPublishJobID);
 
-		$transitionOne = new WorkflowTransition();
-		$transitionOne->Title = 'Step One T1';
-		$transitionOne->ActionID = $stepOne->ID;
-		$transitionOne->NextActionID = $stepTwo->ID;
-		$transitionOne->write();
+		// Set a past publish date
+		$page->PublishOnDate = '2010-01-01 00:00:00';
+		$page->write();
 
-		return $definition;
+		// We should still have a job to publish this page, but not unpublish
+		$this->assertNotEmpty($page->PublishJobID);
+		$this->assertEmpty($page->UnPublishJobID);
+
+		// Check that this job is set for immediate run
+		$publish = strtotime($page->PublishJob()->StartAfter);
+		$this->assertEmpty($publish);
+
+		// Now add an expiry date in the past, but after the current publish job,
+		// and ensure that this correctly overrides the open publish request
+		$page->UnPublishOnDate = '2010-01-02 00:00:00';
+		$page->write();
+
+		// Now we should have an unpublish job, but the publish job is noticably absent
+		$this->assertEmpty($page->PublishJobID);
+		$this->assertNotEmpty($page->UnPublishJobID);
+
+		// Check that this unpublish job is set for immediate run
+		$unpublish = strtotime($page->UnPublishJob()->StartAfter);
+		$this->assertEmpty($unpublish);
+
+		// Now add an expiry date in the future, and ensure that we get the correct combination of
+		// publish and unpublish jobs
+		$page->UnPublishOnDate = '2015-01-01 12:00:00';
+		$page->write();
+
+		// Both jobs exist
+		$this->assertNotEmpty($page->PublishJobID);
+		$this->assertNotEmpty($page->UnPublishJobID);
+
+		// Check that this unpublish job is set for immediate run and the unpublish for future
+		$publish = strtotime($page->PublishJob()->StartAfter);
+		$unpublish = strtotime($page->UnPublishJob()->StartAfter);
+		$this->assertEmpty($publish); // for immediate run
+		$this->assertGreaterThan(strtotime(SS_Datetime::now()->getValue()), $unpublish); // for later run
+	}
+
+	/**
+	 * Test that a page with a past unpublish date creates the correct jobs
+	 */
+	public function testPastUnPublishThenPublish() {
+		$page = new Page();
+		$page->Title = 'My Page';
+		$page->write();
+
+		// No publish
+		$this->assertEmpty($page->PublishJobID);
+		$this->assertEmpty($page->UnPublishJobID);
+
+		// Set a past unpublish date
+		$page->UnPublishOnDate = '2010-01-01 00:00:00';
+		$page->write();
+
+		// We should still have a job to unpublish this page, but not publish
+		$this->assertEmpty($page->PublishJobID);
+		$this->assertNotEmpty($page->UnPublishJobID);
+
+		// Check that this job is set for immediate run
+		$unpublish = strtotime($page->UnPublishJob()->StartAfter);
+		$this->assertEmpty($unpublish);
+
+		// Now add an publish date in the past, but after the unpublish job,
+		// and ensure that this correctly overrides the open unpublish request
+		$page->PublishOnDate = '2010-01-02 00:00:00';
+		$page->write();
+
+		// Now we should have an publish job, but the unpublish job is noticably absent
+		$this->assertNotEmpty($page->PublishJobID);
+		$this->assertEmpty($page->UnPublishJobID);
+
+		// Check that this publish job is set for immediate run
+		$publish = strtotime($page->PublishJob()->StartAfter);
+		$this->assertEmpty($publish);
+
+		// Now add a publish date in the future, and ensure that we get the correct combination of
+		// publish and unpublish jobs
+		$page->PublishOnDate = '2015-01-01 12:00:00';
+		$page->write();
+
+		// Both jobs exist
+		$this->assertNotEmpty($page->PublishJobID);
+		$this->assertNotEmpty($page->UnPublishJobID);
+
+		// Check that this unpublish job is set for immediate run and the unpublish for future
+		$publish = strtotime($page->PublishJob()->StartAfter);
+		$unpublish = strtotime($page->UnPublishJob()->StartAfter);
+		$this->assertEmpty($unpublish); // for immediate run
+		$this->assertGreaterThan(strtotime(SS_Datetime::now()->getValue()), $publish); // for later run
 	}
 }
