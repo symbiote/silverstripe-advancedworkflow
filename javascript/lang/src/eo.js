9b4c04b10bb6929a86ea8f56de676da9a048383e
Damian Mooyman <damian@silverstripe.com>
2014-11-19 11:44:30 +1300

    Update translations

--- javascript/lang/src/eo.js
+++ javascript/lang/src/eo.js
@@ -1,7 +1,7 @@
 {
-    "Workflow.DeleteQuestion": "Are you sure you want to permanently delete this?",
-    "Workflow.EMBARGOMESSAGETIME": "Saved drafts of this page will auto publish today at <a>%s</a>",
-    "Workflow.EMBARGOMESSAGEDATE": "Saved drafts of this page will auto publish on <a>%s</a>",
-    "Workflow.EMBARGOMESSAGEDATETIME": "Saved drafts of this page will auto publish on <a>%s at %s</a>",
-    "Workflow.ProcessError": "Could not process workflow"
+    "Workflow.DeleteQuestion": "Ĉi vi vere volas porĉiame forigi ĉi tiun dosieron?",
+    "Workflow.EMBARGOMESSAGETIME": "Konservitaj malnetoj de la paĝo aŭtomate publikiĝos hodiaŭ je  <a>%s</a>",
+    "Workflow.EMBARGOMESSAGEDATE": "Konservitaj malnetoj de la paĝo aŭtomate publikiĝos je <a>%s</a>",
+    "Workflow.EMBARGOMESSAGEDATETIME": "Konservitaj malnetoj de la paĝo aŭtomate publikiĝos ĉe <a>%s je %s</a>",
+    "Workflow.ProcessError": "Ne povis trakti la laborfluon"
 }
