291582cc33b24abe697690b582a491219dbca1dc
Guy Marriott <guy@scopey.co.nz>
2019-05-09 15:44:58 +1200

    Update translations

--- client/lang/src/es.js
+++ client/lang/src/es.js
@@ -1,7 +1,7 @@
 {
     "Workflow.DeleteQuestion": "¿Está seguro de querer borrar esto permanentemente?",
-    "Workflow.EMBARGOMESSAGETIME": "Los borradores guardados de esta página se publicarán automáticamente hoy a las <a>%s</a>",
     "Workflow.EMBARGOMESSAGEDATE": "Los borradores guardados de esta página se publicarán automáticamente el <a>%s</a>",
     "Workflow.EMBARGOMESSAGEDATETIME": "Los borradores guardados de esta página se publicarán automáticamente el <a>%s a las %s</a>",
+    "Workflow.EMBARGOMESSAGETIME": "Los borradores guardados de esta página se publicarán automáticamente hoy a las <a>%s</a>",
     "Workflow.ProcessError": "No se pudo procesar el flujo de trabajo"
 }
--- client/lang/src/hr.js
+++ client/lang/src/hr.js
@@ -1,7 +1,7 @@
 {
     "Workflow.DeleteQuestion": "Da li ste sigurni da želite trajno obrisati ovo?",
-    "Workflow.EMBARGOMESSAGETIME": "Spremljeni draft ove stranice će biti automatski objavljen danas u <a>%s</a>",
     "Workflow.EMBARGOMESSAGEDATE": "Spremljeni draft ove stranice će biti automatski objavljen <a>%s</a>",
     "Workflow.EMBARGOMESSAGEDATETIME": "Spremljeni draft ove stranice će biti automatski objavljen <a>%s u %s</a>",
+    "Workflow.EMBARGOMESSAGETIME": "Spremljeni draft ove stranice će biti automatski objavljen danas u <a>%s</a>",
     "Workflow.ProcessError": "Ne mogu obraditi proces \"workflow\""
 }
