c9238e5e59ea7154e495f7cc2c96e34c56d3e1ea
Steve Boyd <emteknetnz@gmail.com>
2026-08-19 12:17:25 +1200

    TLN Update translations

--- client/lang/src/en.js
+++ client/lang/src/en.js
@@ -5,7 +5,6 @@
     "Workflow.EMBARGOMESSAGEDATETIME": "Saved drafts of this page will auto publish on <a>%s at %s</a>",
     "Workflow.EMBARGOMESSAGETIME": "Saved drafts of this page will auto publish today at <a>%s</a>",
     "Workflow.ProcessError": "Could not process workflow",
-    "Workflow.CreateWorkflow": "Create workflow",
     "WorkflowField.AddTransition": "Add transition to {action}",
     "WorkflowField.EditSpecificAction": "Edit {action}",
     "WorkflowField.EditTransition": "Edit transition {action}"
